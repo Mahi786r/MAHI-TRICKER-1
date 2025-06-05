016df1d697f9b60925338303c9c87534636ca638
Mahi786r <mahi288rajpoot@gmail.com>
2025-06-05 21:33:22 +0500

    Update Lund.js

--- Priyansh/commands/Lund.js
+++ Priyansh/commands/Lund.js
@@ -13,11 +13,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
   if(react.includes("lund") ||
-     react.includes("LUND") || react.includes("LND") || react.includes("lnd") ||
+     react.includes("LUND") || react.includes("Lol") || react.includes("lnd") ||
 react.includes("laude") ||
 react.includes("loda")) {
     var msg = {
-        body: `लोहे का लन्ड है मेरा निकल के हाथ में दे दूंगा।😐😒👈`,
+        body: `𝐀𝐛𝐲 𝐁𝐡𝐧 𝐤 𝐋𝐨𝐫𝐲 𝐓𝐫𝐚 𝐋𝐮𝐧 𝐊𝐚𝐭 𝐊 𝐇𝐚𝐭𝐡 𝐌 𝐏𝐤𝐫𝐚 𝐃𝐨𝐧𝐠𝐚 𝐀𝐛 𝐆𝐚𝐥𝐢 𝐃😐😒👈`,
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😆", event.messageID, (err) => {}, true)
